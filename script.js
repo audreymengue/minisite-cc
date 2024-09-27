1c7bf517cdea4204e88865a7f707bb3afa3f8822
lamengs <mac@macs-MacBook-Pro-2.local>
2024-09-27 17:07:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js
+
+function saveContact() {
+    alert('Save Contact button clicked!');
+}
+
+function exchangeContact() {
+    alert('Exchange Contact button clicked!');
+}
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length ;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+    setInterval(nextSlide, 5000); // Change slide every 3 seconds
+});
